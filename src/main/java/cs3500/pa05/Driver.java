8e4646a711cec37bdf38ce9ba49f62aef4c83156
drewhermes <hermes.a@northeastern.edu>
2023-06-15 16:55:21 -0400

    Upload wireframe and UML

---
+++ src/main/java/cs3500/pa05/Driver.java
@@ -0,0 +1,20 @@
+package cs3500.pa05;
+
+import cs3500.pa05.controller.ControlJournal;
+import javafx.application.Application;
+
+/**
+ * Starting class of the project
+ */
+public class Driver {
+  /**   
+   * Entry point for the gradle project
+   *
+   * @param args command line arguments for the program
+   */
+  public static void main(String[] args) {
+
+    Application.launch();
+
+  }
+}
---
+++ src/main/java/cs3500/pa05/controller/ControlJournal.java
@@ -0,0 +1,10 @@
+package cs3500.pa05.controller;
+
+/**
+ * Controller interface for the Java Journal
+ */
+public interface ControlJournal {
+
+  
+
+}
