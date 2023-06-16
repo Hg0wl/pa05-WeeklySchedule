52635ba68ebab93a44deeadd290198130a6a2e36
drewhermes <hermes.a@northeastern.edu>
2023-06-16 18:49:04 -0400

    Changes by thomas, Implements event handlers. Fixes #3. Fixes #4. Fixes #5. Fixes #6. Fixes #7

---
+++ src/main/java/cs3500/pa05/controller/eventHandlers/EnterFileEventHandler.java
@@ -0,0 +1,21 @@
+package cs3500.pa05.controller.eventHandlers;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+/**
+ * Represents an event handler for creating an event
+ */
+public class EnterFileEventHandler implements EventHandler<ActionEvent> {
+
+  /**
+   * Handles the event
+   *
+   * @param event the event which occurred
+   */
+  @Override
+  public void handle(ActionEvent event) {
+    //TODO
+  }
+
+}
---
+++ src/main/java/cs3500/pa05/controller/eventHandlers/SaveFileEventHandler.java
@@ -0,0 +1,21 @@
+package cs3500.pa05.controller.eventHandlers;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+/**
+ * Represents an event handler for saving a file
+ */
+public class SaveFileEventHandler implements EventHandler<ActionEvent> {
+
+  /**
+   * Handles the event
+   *
+   * @param event the event which occurred
+   */
+  @Override
+  public void handle(ActionEvent event) {
+    //TODO
+  }
+
+}
