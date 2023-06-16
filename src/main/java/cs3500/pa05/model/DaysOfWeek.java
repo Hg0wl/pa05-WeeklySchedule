c865cb1e2a9124cb2bbddd7a2f8daadacacb2395
drewhermes <hermes.a@northeastern.edu>
2023-06-16 17:15:21 -0400

    Implements Enums. Fixes #2

---
+++ src/main/java/cs3500/pa05/model/DaysOfWeek.java
@@ -0,0 +1,31 @@
+package cs3500.pa05.model;
+
+/**
+ * All possible days of the week
+ */
+public enum DaysOfWeek {
+  /**
+   * Represents the day Sunday
+   */
+  SUNDAY,
+  /**
+   * Represents the day Monday
+   */
+  MONDAY,
+  /**
+   * Represents the day Tuesday
+   */
+  TUESDAY,
+  /**
+   * Represents the day Wednesday
+   */
+  WEDNESDAY,
+  /**
+   * Represents the day Thursday
+   */
+  THURSDAY,
+  /**
+   * Represents the day Friday
+   */
+  FRIDAY
+}
