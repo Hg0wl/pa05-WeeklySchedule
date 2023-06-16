f33c447f4a836b59cf04f8ab6c6976a2aafda256
drewhermes <hermes.a@northeastern.edu>
2023-06-16 17:15:01 -0400

    Changes by thomas, Implements Enums. Fixes #2

---
+++ src/main/java/cs3500/pa05/model/CompleteStatus.java
@@ -0,0 +1,15 @@
+package cs3500.pa05.model;
+
+/**
+ * Represents completion status of a task
+ */
+public enum CompleteStatus {
+  /**
+   * Represents a complete task
+   */
+  COMPLETE,
+  /**
+   * Represents a task that is incomplete
+   */
+  INCOMPLETE
+}
